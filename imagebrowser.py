126004f02d6ec022a7d48597c5763f78ffbef48e
Toolanto <antonio@antonio.(none)>
2013-04-28 16:42:36 +0200

    translate images done, to do improve

--- imagebrowser.py
+++ imagebrowser.py
@@ -39,35 +39,41 @@ class ImageDisplay:
     display.flip()
       
 if __name__=='__main__':
-  time.delay(1000)
   path = sys.argv[1]
+  haarcascade = sys.argv[2]
   obj = ImageDisplay(800,600)
   running = True
   start = 0
   offsset = 0
   move_x = 0
   image_i= 0
+  index = 1
   first = True
   imgs = obj.load_images(path,"jpg")
   hd = handdetection.HandDetection("detection",800,600)
-  handcascade = hd.loadHaarcascade("aGest.xml")
+  handcascade = hd.loadHaarcascade(haarcascade)
   while running:
+    time.wait(100)
+    print image_i,index
     result = hd.detectHand(handcascade)
     #detect hand fist
     if result[1] == 0 and first:
       start = result[2]
       first = False
     if result[1] != 0 and result[1] != None:
-      offset = hd.width - result[2] - start - 700
+      offsset = hd.width - result[2] - start - 700
       start = result[2]
-      move_x = offset
+      move_x = offsset
     if result[1] == None or result[2] == None:
       first = True
       move_x = 0
-    if move_x > 20 and image_i < len(imgs) -1:
-      image_i+=1
-    elif move_x < 20 and image_i > 0:
-      image_i-=1
+    if move_x > 20 and image_i < len(imgs) -1 and not(first):
+      index = index + 1
+      print ">20 - {0}".format(image_i)
+    elif move_x < 20 and image_i > 0 and not(first):
+      index = index - 1
+      print "<20 - {0}".format(image_i)
+    image_i = index
     obj.display_image(path,"jpg",image_i,move_x,0)
     k = hd.showImage("detection", result[0])
     if len(event.get(QUIT))>0 or k == 102:
